c5ad17133e6e9d49eb023e52b822f47a73f6e272
SubiNesa <subinesa@gmail.com>
2024-04-26 18:58:23 +0200

    Init

---
+++ src/routes/Table.svelte
@@ -0,0 +1,52 @@
+<script>
+    const size = 10;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+</script>
+
+<div class="resp-table">
+    <div class="resp-table-body">
+        <div class="resp-table-row"> 
+            <div class="table-body-cell header">
+        
+            </div>
+            {#each alphabet.slice(0, size - 1) as al, a}
+            <div class="table-body-cell header">
+                {al} 
+            </div>
+            {/each}
+        </div>
+        {#each [...Array(size).keys()] as row, r}
+        <div class="resp-table-row"> 
+            {#each [...Array(size).keys()] as cell, c}
+            <div class="table-body-cell" class:header={cell == 0}>
+                {c == 0 ? r : alphabet[c - 1] + ' ' +  r} 
+            </div>
+            {/each}
+        </div>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .resp-table {
+        width: 100%;
+        display: table;
+    }
+    .resp-table-body{
+        display: table-row-group;
+    }
+    .resp-table-row{
+        display: table-row;   
+    }
+    .table-body-cell{
+        display: table-cell;
+        border: 1px solid #dddddd;
+        padding: 8px;
+        line-height: 2;
+        width: 15px;
+    }
+    .table-body-cell.header{
+        background-color: gainsboro;        
+    }
+</style>
+
---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Table from './../Table.svelte';
+</script>
+
+<main>
+    <div class="flex-row-item">1</div>
+    <div class="flex-row-item"><Table></Table></div>
+    <div class="flex-row-item">3</div>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+    main > .flex-row-item {
+        flex: 1 1 30%; /*grow | shrink | basis */
+        height: 100px;
+    }
+</style>
